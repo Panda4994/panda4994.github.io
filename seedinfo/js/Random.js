a01bb75da4bb9b357e9e7caebf84698d4e2c64c1
Panda4994 <allid@live.de>
2017-08-13 00:01:40 +0200

    Worked a bit on everything (except css) and added unhash-functionallity

--- seedinfo/js/Random.js
+++ seedinfo/js/Random.js
@@ -1,93 +1,95 @@
-var MULT = bigInt("0x5DEECE66D");
-var ADD = bigInt("0xB");
-var MASK48 = bigInt.one.shiftLeft(48).subtract(1);
-var MASK32 = bigInt.one.shiftLeft(32).subtract(1);
-var MASK31 = bigInt.one.shiftLeft(31).subtract(1);
-var MASK64 = bigInt.one.shiftLeft(64).subtract(1);
-var MASK63 = bigInt.one.shiftLeft(63).subtract(1);
+var javaRandom = (function(seed) {
+	var MULT = bigInt("0x5DEECE66D");
+	var ADD = bigInt("0xB");
+	var MASK48 = bigInt.one.shiftLeft(48).minus(1);
+	var MASK32 = bigInt.one.shiftLeft(32).minus(1);
+	var MASK31 = bigInt.one.shiftLeft(31).minus(1);
+	var MASK64 = bigInt.one.shiftLeft(64).minus(1);
+	var MASK63 = bigInt.one.shiftLeft(63).minus(1);
 
-function Random(seed) {
-	this.seed = bigInt(seed).xor(MULT).and(MASK48);
-}
+	function RandomClass(seed) {
+		return new Random(seed);
+	}
 
-Random.prototype.setSeed = function(seed) {
-	this.seed = bigInt(seed).xor(MULT).and(MASK48);
-}
+	RandomClass.fromLong = function(l) {
+		var i1 = l.shiftRight(32).and(MASK32);
+		var i2 = l.and(MASK32);
+		var front = i1.shiftLeft(16);
+		for (var i=bigInt.zero; i.lt(bigInt.one.shiftLeft(16)); i = i.add(1)) {
+			var seed = front.or(i);
+			var i22 = seed.times(MULT).add(ADD).and(MASK48).shiftRight(16);
+			if (i22.eq(i2)) {
+				var ret = new Random();
+				ret.setOrgSeed(seed);
+				ret.previous();
+				return ret;
+			}
+		}
+		return undefined;
+	}
 
-Random.prototype.getSeed = function() {
-	return this.seed.xor(MULT);
-}
+	function Random(seed) {
+		this.setSeed(bigInt(seed));
+	}
 
-Random.prototype.setOrgSeed = function(seed) {
-	this.seed = bigInt(seed);
-}
+	Random.prototype.setSeed = function(seed) {
+		this.seed = bigInt(seed).xor(MULT).and(MASK48);
+	}
+	Random.prototype.getSeed = function() {
+		return this.seed.xor(MULT);
+	}
 
-Random.prototype.getOrgSeed = function() {
-	return this.seed;
-}
+	Random.prototype.setOrgSeed = function(seed) {
+		this.seed = bigInt(seed);
+	}
 
-Random.prototype.next = function(bits) {
-	this.seed = this.seed.times(MULT).add(ADD).and(MASK48);
-	return fixInt(this.seed.shiftRight(48 - bits).and(MASK32));
-}
+	Random.prototype.getOrgSeed = function() {
+		return this.seed;
+	}
 
-Random.prototype.nextInt = function() {
-	return this.next(32);
-}
+	Random.prototype.next = function(bits) {
+		this.seed = this.seed.times(MULT).add(ADD).and(MASK48);
+		return fixInt(this.seed.shiftRight(48 - bits).and(MASK32));
+	}
 
-Random.prototype.previous = function() {
-	var p1 = bigInt.zero;
-	for (var i=1; i<=48; i++) {
-		var mask = bigInt.one.shiftLeft(i).minus(1);
-		var p2 = p1.times(MULT).add(ADD).and(mask);
-		if (p2.neq(this.seed.and(mask))) {
-			p1 = p1.or(bigInt.one.shiftLeft(i-1));
+	Random.prototype.previous = function() {
+		var p1 = bigInt.zero;
+		for (var i=1; i<=48; i++) {
+			var mask = bigInt.one.shiftLeft(i).minus(1);
+			var p2 = p1.times(MULT).add(ADD).and(mask);
+			if (p2.neq(this.seed.and(mask))) {
+				p1 = p1.or(bigInt.one.shiftLeft(i-1));
+			}
+		}
+		this.seed = p1;
+	}
+
+	Random.prototype.nextInt = function() {
+		return this.next(32);
+	}
+
+	Random.prototype.nextLong = function() {
+		return fixLong(this.next(32).shiftLeft(32).add(this.next(32)));
+	}
+
+	function fixInt(i) {
+		var ret = i.and(MASK32);
+		if (ret.gt(MASK31)) {
+			return ret.xor(MASK32).add(1).negate();
+		} else {
+			return ret;
 		}
 	}
-	this.seed = p1;
-}
 
-Random.prototype.nextLong = function() {
-	return fixLong(this.next(32).shiftLeft(32).add(this.next(32)));
-}
-
-
-function fixInt(i) {
-	var ret = i.and(MASK32);
-	if (ret.gt(MASK31)) {
-		return ret.xor(MASK32).add(1).negate();
-	} else {
-		return ret;
-	}
-}
-
-function fixLong(i) {
-	var ret = i.and(MASK64);
-	if (ret.gt(MASK63)) {
-		return ret.xor(MASK64).add(1).negate();
-	} else {
-		return ret;
-	}
-}
-
-function stringHash(str) {
-	var ret = bigInt.zero;
-	for (var i=0; i<str.length; i++) {
-		ret = ret.times(31).add(str.charCodeAt(i));
-	}
-	return fixInt(ret);
-}
-
-function fromLong(l) {
-	var i1 = l.shiftRight(32).and(MASK32);
-	var i2 = l.and(MASK32);
-	var front = i1.shiftLeft(16);
-	for (var i=bigInt.zero; i.lt(bigInt.one.shiftLeft(16)); i = i.add(1)) {
-		var seed = front.or(i);
-		var i22 = seed.times(MULT).add(ADD).and(MASK48).shiftRight(16);
-		if (i22.eq(i2)) {
-			return seed;
+	function fixLong(i) {
+		var ret = i.and(MASK64);
+		if (ret.gt(MASK63)) {
+			return ret.xor(MASK64).add(1).negate();
+		} else {
+			return ret;
 		}
 	}
-	return undefined;
-}
+
+	return RandomClass;
+})();
+
--- seedinfo/js/SeedInfo.js
+++ seedinfo/js/SeedInfo.js
@@ -1,41 +1,75 @@
-var INT_MIN = bigInt.one.shiftLeft(31).negate();
-var INT_MAX = bigInt.one.shiftLeft(31).minus(1);
-var LONG_MIN = bigInt.one.shiftLeft(63).negate();
-var LONG_MAX = bigInt.one.shiftLeft(63).minus(1);
+var seedInfo = (function(str) {
+	var INT_MIN = bigInt.one.shiftLeft(31).negate();
+	var INT_MAX = bigInt.one.shiftLeft(31).minus(1);
+	var LONG_MIN = bigInt.one.shiftLeft(63).negate();
+	var LONG_MAX = bigInt.one.shiftLeft(63).minus(1);
 
-var parseLong = function(seed) {
-	var ret = bigInt(seed); // TODO Make sure Java-Rules apply
-	return ret;
-}
-
-var checkSeed = function(str) {
-	res = new Object();
-	res.enteredString = false;
-	res.outOfRange = false;
-	if (str.match("^[+-]?[0-9]+$") != null) {
-		res.seed = parseLong(str);
-		if (res.seed.leq(LONG_MIN) || res.seed.geq(LONG_MAX)) {
+	function checkSeed(str) {
+		res = new Object();
+		res.enteredString = false;
+		res.outOfRange = false;
+		if (str.match("^[+-]?[0-9]+$") != null) {
+			res.seed = bigInt(str);
+			if (res.seed.leq(LONG_MIN) || res.seed.geq(LONG_MAX)) {
+				res.seed = stringHash(str);
+				res.outOfRange = true;
+			}
+		} else {
 			res.seed = stringHash(str);
-			res.outOfRange = true;
+			res.enteredString = true;
 		}
-	} else {
-		res.seed = stringHash(str);
-		res.enteredString = true;
+		res.byString = false;
+		res.byRandom = false;
+		res.byNumber = true;
+		res.time = undefined;
+		
+		if (res.seed.geq(INT_MIN) && res.seed.leq(INT_MAX)) {
+			res.byString = true;
+			res.strings = generateStrings(res.seed, "<");
+			res.strings = res.strings.concat(generateStrings(res.seed, "`"));
+		}
+		if (res.seed.eq(bigInt.zero)) {
+			res.byNumber = false;
+		}
+		res.rand = javaRandom.fromLong(res.seed);
+		if (res.rand != undefined) {
+			res.byRandom = true;
+			res.time = res.rand.getSeed();
+		}
+		return res;
 	}
-	res.byString = false;
-	res.byRandom = false;
-	res.byNumber = true;
-	res.time = undefined;
-	
-	if (res.seed.geq(INT_MIN) && res.seed.leq(INT_MAX)) {
-		res.byString = true;
+
+	function stringHash(str) {
+		var ret = bigInt.zero;
+		for (var i=0; i<str.length; i++) {
+			ret = ret.times(31).add(str.charCodeAt(i));
+		}
+		return numHelper.fixInt(ret);
 	}
-	if (res.seed.eq(bigInt.zero)) {
-		res.byNumber = false;
+
+	function generateStrings(seed, start) {
+		var res = [];
+		var base = "";
+		while (res.length < 5) {
+			base += start;
+			var dist = numHelper.breakInt(seed).minus(numHelper.breakInt(stringHash(base)));
+			while (dist.lt(bigInt.zero)) {
+				dist = dist.add(bigInt.one.shiftLeft(32));
+			}
+			while (bigInt(31).pow(base.length).gt(dist)) {
+				var next = "";
+				var tmp = dist;
+				for (var i=0; i<base.length; i++) {
+					var c = base.charCodeAt(i) + tmp.mod(31).toJSNumber();
+					tmp = tmp.divide(31);
+					next = String.fromCharCode(c) + next;
+				}
+				res.push(next);
+				dist = dist.add(bigInt.one.shiftLeft(32));
+			}
+		}
+		return res;
 	}
-	res.time = fromLong(res.seed);
-	if (res.time != undefined) {
-		res.byRandom = true;
-	}
-	return res;
-}
+
+	return checkSeed;
+})();
